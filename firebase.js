c1baed7b3c9358811267f7907d6839fd9987826d
sohobidin06-byte <sohobidin06@icloud.com>
2026-03-05 16:37:08 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,8 @@
-// Firebase конфигурация
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://germangame-b5626-default-rtdb.firebaseio.com/",
-  projectId: "YOUR_PROJECT_ID",
+  apiKey: "СЕНИН_API_KEY",
+  authDomain: "СЕНИН_PROJECT.firebaseapp.com",
+  databaseURL: "СЕНИН_DATABASE_URL",
+  projectId: "СЕНИН_PROJECT_ID",
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
