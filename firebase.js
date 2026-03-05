ae1e91a32593164cbab71466c002799b611eb5eb
sohobidin06-byte <sohobidin06@icloud.com>
2026-03-05 13:21:18 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,9 @@
-
+// База колдонулса гана
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT.firebaseapp.com",
+    databaseURL: "https://YOUR_PROJECT.firebaseio.com",
+    projectId: "YOUR_PROJECT",
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
